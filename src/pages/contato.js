91acc9f2139d1ecda1a328975bf6ab762d68fe95
Hermany13 <hermanyvieiratj@gmail.com>
2019-12-22 17:09:36 -0300

    Styles and Profile image added

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import profile from "./profile.jpg";
+
+
 
 const Contato = () => (
     <div className="title">
+        <img src={profile} style={logo}/>
         <h2>Hermany Vieira Oliveira</h2>        
         <p><span style={spa}>E-mail:</span> hermanyvieiratj@gmail.com</p>
         <p><span style={spa}>GitHub:</span><a href="https://github.com/Hermany13" style={lin}> https://github.com/Hermany13</a></p>
     </div>
 );
+
 
 const spa = {
     fontWeight: "bold",
@@ -14,6 +19,13 @@ const spa = {
 
 const lin = {
     cursor: "pointer"
+}
+
+const logo = {
+    border: '3px solid #66B3FF',
+    width: "100px",
+    borderRadius: "50px",
+    marginTop: "15px",
 }
 
 export default Contato;
